f16f39cf238ff7b7758dcfeb669123fdb9abed6e
Danny Lee <dreamer@panix.com>
2019-10-26 10:52:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if (someValue < 400)
+    return 'This one is on me!';
+  if (someValue > 2000 && someValue <= 2500)
+    return 'I will gladly take your thirty bucks.';
+  if (someValue > 2500)
+    return 'No can do.';
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(destination){
   // Write your code here!
+  return (destination === 'NYC' ? "Ok, sounds good." : "No go.");
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip == 'generous') {
+    return 'Thank you so much.';
+  } else if (tip == 'not as generous') {
+    return 'Thank you.';
+  } else {
+    return 'Bye.';
+  }
 }
