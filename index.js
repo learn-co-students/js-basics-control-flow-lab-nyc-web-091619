624cf99392b53cbc4e9582652171fd48b5bcf88c
H. Turcan Cicek <htcicek@users.noreply.github.com>
2019-10-27 12:45:53 -0400

    met all specs

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+
+  if (someValue > 2500) {
+    return "No can do."
+  } else if(someValue > 2000) {
+    return "I will gladly take your thirty bucks."
+  } else if(someValue <= 400){
+    return "This one is on me!"
+  } else {
+    return "money"
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityName){
+  let response;
+  cityName === "NYC" ? response = "Ok, sounds good." : response = "No go."
+  return response;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let response;
+  switch (tip) {
+    case 'generous':
+      response = 'Thank you so much.'
+      break;
+    case 'not as generous':
+    response = 'Thank you.'
+    break;
+    default:
+      response = "Bye."
+      break;
+  }
+  return response;
 }
