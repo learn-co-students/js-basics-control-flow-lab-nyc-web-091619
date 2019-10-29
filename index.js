8aa145dc6cb399aeb2b0d5b3d8e6338b8164e52f
divbeech <dyadav1006@gmail.com>
2019-10-29 13:21:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let greeting;
+
+   if (distance <= 400) {
+     greeting = `This one is on me!`;
+   }
+   else if (distance > 2500) {
+     greeting = 'No can do.';
+   }
+   else if (distance > 2000) {
+     greeting = 'I will gladly take your thirty bucks.';
+   }
+   return greeting;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return (city === 'NYC') ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  let response = '';
+  switch (tip) {
+    case 'generous':
+      response = 'Thank you so much.';
+       break;
+    case 'not as generous':
+      response = 'Thank you.';
+      break;
+    default:
+    response = 'Bye.';
+    break;
+  }
+    return response;
   // Write your code here!
 }
